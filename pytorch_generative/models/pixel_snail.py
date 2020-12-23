c7a7fa1226aff02177f344c29290a78937a3ab19
eyalbetzalel <eyalbatzul@gmail.com>
2020-12-23 18:31:31 +0200

    sampling

--- pytorch_generative/models/pixel_snail.py
+++ pytorch_generative/models/pixel_snail.py
@@ -282,7 +282,7 @@ def reproduce(n_epochs=457, batch_size=128, log_dir="/tmp/run", device="cuda", d
 
         return loss
 
-
+    _model = model.to(device)
     trainer = trainer.Trainer(
         model=model,
         loss_fn=loss_fn,
@@ -293,6 +293,7 @@ def reproduce(n_epochs=457, batch_size=128, log_dir="/tmp/run", device="cuda", d
         log_dir=log_dir,
         device=device,
         sample_epochs = 1,
-        sample_fn=model.to(device)().sample(out_shape= [1,1,1024,1]),
+        sample_fn=_model.sample(out_shape= [1,1,1024,1]),
     )
+
     trainer.interleaved_train_and_eval(n_epochs)
