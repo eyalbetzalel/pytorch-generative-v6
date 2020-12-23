2cc8914c9e02b3777464202cf3db225e41612e1e
eyalbetzalel <eyalbatzul@gmail.com>
2020-12-23 18:10:52 +0200

    sampling

--- pytorch_generative/models/base.py
+++ pytorch_generative/models/base.py
@@ -54,8 +54,9 @@ class AutoregressiveModel(nn.Module):
                     out = self.forward(conditioned_on)
                     out = out[:,:,:,None]
                     out = out[:, :, row, col]
+
+                    import ipdb; ipdb.set_trace()
                     if torch.min(out) > 0:
-                        import ipdb; ipdb.set_trace()
                         out = self._sample_fn(out).view(n, c)
                         conditioned_on[:, :, row, col] = torch.where(
                             conditioned_on[:, :, row, col] < 0,
