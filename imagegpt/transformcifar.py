a27c5df6cf7aed16a7d6a40234dcd954db23ceba
eyalbetzalel <eyalbatzul@gmail.com>
2020-12-31 18:30:56 +0200

    sampling

--- imagegpt/transformcifar.py
+++ imagegpt/transformcifar.py
@@ -26,11 +26,13 @@ def transform_cluster_to_image(samples):
     ############################### NEW ########################################################
     data_tor = torch.reshape(torch.from_numpy(samples2), [-1, 32, 32])
     clusters = torch.from_numpy(np.load(pathToCluster)).float()
-    import ipdb; ipdb.set_trace()
-    sample_new = torch.reshape(torch.round(127.5 * (torch.index_select(clusters, 0, data_tor.long().flatten()) + 1.0)),[data_tor.shape[0], 3, 32, 32]).to('cuda')
-    # sample = torch.reshape(torch.round(127.5 * (clusters[data.long()] + 1.0)), [data.shape[0], 3, 32, 32]).to('cuda')
-    yos = torch.eq(sample,sample_new)
 
+    # sample_new = torch.reshape(torch.round(127.5 * (torch.index_select(clusters, 0, data_tor.long().flatten()) + 1.0)),[data_tor.shape[0], 3, 32, 32]).to('cuda')
+    sample_new = torch.round(127.5 * (clusters[data.long()] + 1.0))
+    sample_new = sample_new.permute(0, 3, 1, 2)
+    yos = torch.eq(samples,sample_new)
+    import ipdb;
+    ipdb.set_trace()
     return samples
 
 def load_data(data_path):
