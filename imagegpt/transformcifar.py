e865d011c5b0e5f797bc0c0e700725966e3432d1
eyalbetzalel <eyalbatzul@gmail.com>
2020-12-31 17:26:56 +0200

    sampling

--- imagegpt/transformcifar.py
+++ imagegpt/transformcifar.py
@@ -14,14 +14,13 @@ global clusters
 clusters = torch.from_numpy(np.load(pathToCluster)).float()
 
 def transform_cluster_to_image(data):
-    data = torch.reshape(torch.from_numpy(data), [-1, 32, 32])
+    # data = torch.reshape(torch.from_numpy(data), [-1, 32, 32])
     # [np.reshape(np.rint(127.5 * (clusters[s] + 1.0)), [32, 32, 3]).astype(np.uint8) for s in samples]
-
+    import ipdb; ipdb.set_trace()
     sample_new = torch.reshape(torch.round(127.5 * (torch.index_select(clusters, 0, data.long()) + 1.0)),
                            [data.shape[0], 3, 32, 32]).to('cuda')
     sample = torch.reshape(torch.round(127.5 * (clusters[data.long()] + 1.0)), [data.shape[0], 3, 32, 32]).to('cuda')
-    import ipdb;
-    ipdb.set_trace()
+
 
     return sample
 
