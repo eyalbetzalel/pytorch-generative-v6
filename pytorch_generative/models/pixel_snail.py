d0a256310032e0f1993d03792bee9f08f0025688
eyalbetzalel <eyalbatzul@gmail.com>
2020-12-31 19:14:16 +0200

    sampling

--- pytorch_generative/models/pixel_snail.py
+++ pytorch_generative/models/pixel_snail.py
@@ -188,7 +188,7 @@ class PixelSNAIL(base.AutoregressiveModel):
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~EB~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         # Mapping function from 1-ch cluster to 3-ch RGB images :
         # x = torch.reshape(torch.round(127.5 * (clusters[x.long()] + 1.0)), [x.shape[0] ,3, 32, 32]).to('cuda')
-
+        import ipdb; ipdb.set_trace()
         x = torch.round(127.5 * (clusters[x.long()] + 1.0))
         x = x.permute(0, 3, 1, 2)
         x = x.to('cuda')
