78cd0c36624a262c56badf923cab2498f4cc063f
eyalbetzalel <eyalbatzul@gmail.com>
2020-12-31 19:24:33 +0200

    sampling

--- imagegpt/transformcifar.py
+++ imagegpt/transformcifar.py
@@ -1,12 +1,9 @@
 import numpy as np
-import numpy as np
 import os
 import torch
 from torch.utils import tensorboard
 import torchvision
 import matplotlib.pyplot as plt
-import tensorflow as tf
-
 
 
 pathToCluster = r"/home/dsi/eyalbetzalel/image-gpt/downloads/kmeans_centers.npy"  # TODO : add path to cluster dir
@@ -14,25 +11,13 @@ global clusters
 clusters = torch.from_numpy(np.load(pathToCluster)).float()
 
 def transform_cluster_to_image(samples):
-    samples2 = np.copy(samples)
-    ############################### OLD ########################################################
-    clusters =np.load(pathToCluster)
-    samples = [torch.from_numpy(np.reshape(np.rint(127.5 * (clusters[s.astype(int).tolist()] + 1.0)), [32, 32, 3]).astype(np.float32)) for
-               s in samples]
-    samples = torch.stack(samples)
-    samples = samples.permute(0, 3, 1, 2)
 
-
-    ############################### NEW ########################################################
-    data_tor = torch.reshape(torch.from_numpy(samples2), [-1, 32, 32])
+    data_tor = torch.reshape(torch.from_numpy(samples), [-1, 32, 32])
     clusters = torch.from_numpy(np.load(pathToCluster)).float()
-
-    # sample_new = torch.reshape(torch.round(127.5 * (torch.index_select(clusters, 0, data_tor.long().flatten()) + 1.0)),[data_tor.shape[0], 3, 32, 32]).to('cuda')
     sample_new = torch.round(127.5 * (clusters[data_tor.long()] + 1.0))
     sample_new = sample_new.permute(0, 3, 1, 2)
     yos = torch.eq(samples,sample_new)
-    import ipdb;
-    ipdb.set_trace()
+
     return samples
 
 def load_data(data_path):
@@ -44,11 +29,8 @@ def load_data(data_path):
     teY = np.load(f'{data_path}_teY.npy')
     return (trX, trY), (vaX, vaY), (teX, teY)
 
-
 def plot_images_grid(x: torch.tensor, export_img, title: str = '', nrow=8, padding=2, normalize=True, pad_value=0):
     """Plot 4D Tensor of images of shape (B x C x H x W) as a grid."""
-    import ipdb;
-    ipdb.set_trace()
     grid = torchvision.utils.make_grid(x, nrow=nrow, padding=padding, normalize=normalize, pad_value=pad_value)
     npgrid = grid.cpu().numpy()
     im = np.transpose(npgrid, (1, 2, 0))
@@ -58,12 +40,7 @@ def plot_images_grid(x: torch.tensor, export_img, title: str = '', nrow=8, paddi
 data_path = './cifar10'
 (trX, trY), (vaX, vaY), (teX, teY) = load_data(data_path)
 sample = transform_cluster_to_image(vaX)
-
 log_dir = '/home/dsi/eyalbetzalel/pytorch-generative-v6/image_test/test1.png'
-# _summary_writer = tensorboard.SummaryWriter(log_dir = log_dir, max_queue=100)
-# _summary_writer.add_images('sample',sample,0)
-# _summary_writer.close()
-
 pytorch_tensor = sample[1:48,:,:,:]
 plot_images_grid(pytorch_tensor, export_img=log_dir)
 
