eeec43509c37e9a857c89050d0eaf6565be69012
eyalbetzalel <eyalbatzul@gmail.com>
2020-12-31 13:30:02 +0200

    sampling

---
+++ imagegpt/transformcifar.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def load_data(data_path):
+    trX = np.load(f'{data_path}_trX.npy')
+    trY = np.load(f'{data_path}_trY.npy')
+    vaX = np.load(f'{data_path}_vaX.npy')
+    vaY = np.load(f'{data_path}_vaY.npy')
+    teX = np.load(f'{data_path}_teX.npy')
+    teY = np.load(f'{data_path}_teY.npy')
+    return (trX, trY), (vaX, vaY), (teX, teY)
+
+data_path = '/home/dsi/eyalbetzalel/pytorch-generative-v6/imagegpt/download/'
+(trX, trY), (vaX, vaY), (teX, teY) = load_data(data_path)
+import ipdb; ipdb.set_trace()
