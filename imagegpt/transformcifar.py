a89869ff0129e44578be84140fc460186e11927b
eyalbetzalel <eyalbatzul@gmail.com>
2020-12-31 17:50:00 +0200

    sampling

--- imagegpt/transformcifar.py
+++ imagegpt/transformcifar.py
@@ -48,7 +48,8 @@ def load_data(data_path):
 
 def plot_images_grid(x: torch.tensor, export_img, title: str = '', nrow=8, padding=2, normalize=True, pad_value=0):
     """Plot 4D Tensor of images of shape (B x C x H x W) as a grid."""
-
+    import ipdb;
+    ipdb.set_trace()
     grid = torchvision.utils.make_grid(x, nrow=nrow, padding=padding, normalize=normalize, pad_value=pad_value)
     npgrid = grid.cpu().numpy()
     npgrid = npgrid.astype(np.uint8)
@@ -67,7 +68,7 @@ log_dir = '/home/dsi/eyalbetzalel/pytorch-generative-v6/image_test/test1.png'
 
 pytorch_tensor = sample[1:48,:,:,:]
 plot_images_grid(pytorch_tensor, export_img=log_dir)
-import ipdb; ipdb.set_trace()
+
 
 
 
