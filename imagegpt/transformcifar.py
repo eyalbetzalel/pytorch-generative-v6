ea939739149bfc0bfb4a3392d68245a12891f57f
eyalbetzalel <eyalbatzul@gmail.com>
2020-12-31 15:35:26 +0200

    sampling

--- imagegpt/transformcifar.py
+++ imagegpt/transformcifar.py
@@ -27,7 +27,7 @@ data_path = './cifar10'
 (trX, trY), (vaX, vaY), (teX, teY) = load_data(data_path)
 sample = transform_cluster_to_image(trX)
 
-log_dir = '/home/dsi/eyalbetzalel/pytorch-generative-v6/image_test'
+log_dir = '/home/dsi/eyalbetzalel/pytorch-generative-v6/image_test/test.png'
 # _summary_writer = tensorboard.SummaryWriter(log_dir = log_dir, max_queue=100)
 # _summary_writer.add_images('sample',sample,0)
 # _summary_writer.close()
