71d0d1618e868537988537757b0e41269764fbcd
eyalbetzalel <eyalbatzul@gmail.com>
2021-01-02 13:10:09 +0200

    sampling

--- imagegpt/transformcifar.py
+++ imagegpt/transformcifar.py
@@ -35,6 +35,7 @@ def plot_images_grid(x: torch.tensor, export_img, title: str = '', nrow=8, paddi
     grid = torchvision.utils.make_grid(x, nrow=nrow, padding=padding, normalize=normalize, pad_value=pad_value)
     npgrid = grid.cpu().numpy()
     im = np.transpose(npgrid, (1, 2, 0))
+    import ipdb; ipdb.set_trace()
     plt.imsave(export_img,im)
 
 
